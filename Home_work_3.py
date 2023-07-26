7054f2f4776692b2081ee0b577ed3c381fa16ad9
Vadim <vgdanov@gmail.com>
2023-07-26 11:30:59 +0300

    Line 5 is shortened

--- Home_work_3.py
+++ Home_work_3.py
@@ -2,7 +2,7 @@ unique_symbols = input("Enter your string to count symbols: ")
 result = len(set(unique_symbols))
 print(f"Number of symbols: {result}")
 
-if len(set(unique_symbols)) > 10:
+if result > 10:
     print("Your number has more than 10 unique symbols: " + "True")
 else:
     print("Your number has less than 10 unique symbols: " + "False")
