7b9bbe6ed2232080ed9babf713dfd7c1f22c0a92
Vadim <vgdanov@gmail.com>
2023-07-31 21:59:56 +0300

    Home work 4

---
+++ Home_work_4.py
@@ -0,0 +1,31 @@
+# 1. Напишіть цикл, який буде вимагати від користувача ввести слово,
+# в якому є буква "о" (враховуються як великі так і маленькі).
+# Цикл не повинен завершитися, якщо користувач ввів слово без букви "о".
+
+# input_string = ''
+# while 1:
+#     your_word = input("Type your word: ")
+#     if "o" in your_word:
+#         break
+#     input_string += your_word
+# print("There is an 'o' character in your word(s) ")
+
+# 2. Є list з даними lst1 = ['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum'].
+# Напишіть код, який свормує новий list (наприклад lst2), який містить лише змінні типу стрінг, які присутні в lst1.
+# Данні в лісті можуть бути будь якими
+
+lst1 = str(['1', '2', 3, True, 'False', 5, '6', 7, 8, 'Python', 9, 0, 'Lorem Ipsum'])
+element_1 = "1"
+element_2 = "2"
+element_3 = "False"
+element_4 = "6"
+element_5 = "Python"
+element_6 = 'Lorem Ipsum'
+lst2 = [element_1, element_2, element_3, element_4, element_5, element_6]
+print(lst2)
+
+# 3. Є ліст з числами, порахуйте сумму усіх ПАРНИХ чисел в цьому лісті
+numbers_lst = [70, 22, 3, 44, 87, 1, 30, 10]
+even_nos = [num for num in numbers_lst if num % 2 == 0]
+print("Even numbers in the list: ", even_nos)
+print("Sum of the even numbers: ", len(even_nos))
