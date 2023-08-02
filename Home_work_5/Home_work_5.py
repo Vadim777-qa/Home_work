24a5851b3f5dc089fc856b729518109422bc7dc0
Vadim <vgdanov@gmail.com>
2023-08-02 14:49:59 +0300

    Home work 5 - functions and import

---
+++ Home_work_5/Home_work_5.py
@@ -0,0 +1,17 @@
+#  1. Напишіть функцію, яка приймає два аргументи.
+#  a. Якщо обидва аргумени відносяться до числових типів функція пермножує ці аргументи і повертає результат
+#  b. Якшо обидва аргументи відносяться до типу стрінг функція обʼєднує їх в один і повертає
+#  c. В будь-якому іншому випадку - функція повертає кортеж з двох агрументів
+
+def conditions_for_arguments(one_argument, two_argument):
+    if type(one_argument) == int and type(two_argument) == int:
+        return one_argument * two_argument
+    elif type(one_argument) == str and type(two_argument) == str:
+        return one_argument + " " + two_argument
+    else:
+        test_tuple = (one_argument, two_argument)
+        return test_tuple
+
+
+operations_arguments = conditions_for_arguments(3, 2)
+print(operations_arguments)
---
+++ Home_work_5/import_file_5.py
@@ -0,0 +1,7 @@
+from Home_work_5 import conditions_for_arguments
+
+if __name__ == '__Home_work_5__':
+    result = conditions_for_arguments("You", "Me")
+    print(result)
+
+
